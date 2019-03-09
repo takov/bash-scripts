e4a8196c4324c2fb7bfaa37ec12a845fe8692f60
takov <dognyanov@gmail.com>
2019-03-09 22:57:01 +0200

    initial commit

---
+++ shell_commands.sh
@@ -0,0 +1,27 @@
+
+
+#---cut---
+# Print the trird column of /etc/passwd file. "-f3" - 3rd column. "-d : " - delimiter :
+cut -f3 -d : /etc/passwd
+# Above command but sort the result. Use sort -n if you want to sort numbers
+cut -f1 -d : /etc/passwd | sort | head
+
+#---sort---
+# Show files sizes and sort them
+du -h ~/Documents/ | sort -n
+# Sort the 3rd colum of /etc/passwd file. "-k3" specify column. "-t :" - delimiter :
+sort -n -k3 -t : /etc/passwd
+# Print the 5th line of /etc/passwd file
+
+#---sed---
+sed -n 5p /etc/passwd - Print line 5
+# Delete the 5th line
+sed -i -e '5d' test - Delete line 5
+# Print the 1st columnt of a file
+
+#---awk---
+awk -F : '{print $1}' /etc/passwd
+# Print only users with name takov
+awk -F : '/takov/{print $1}' /etc/passwd 
+# Print the first and the last field 
+awk -F : '/takov/{print $1 $NF}' /etc/passwd
