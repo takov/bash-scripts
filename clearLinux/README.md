1441b048a947f750df52656468634c47c33c2135
Dimitar Ognyanov <takov>
2020-05-06 22:58:36 +0300

    Add script for installing opera and creating symlinks for terminator

---
+++ clearLinux/README.md
@@ -0,0 +1,8 @@
+# ClearLinux scripts
+
+Useful scripts for ClearLinux OS - https://clearlinux.org
+ClearLinux uses its own package manager called swupd. Since it is fairly new a lot of the packages like opera,chrome,terminator etc. are not available. 
+
+"opera.sh" will download latest opera build and it will enable H.264 codecs. 
+
+
---
+++ clearLinux/terminal_ln.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#TODO add installing terminator. The script only creates symlinks 
+base_path=/home/user/software
+target_path=/usr/local
+
+
+
+sudo ln -s $base_path/terminator/share/pixmaps/terminator.png $target_path/share/pixmaps/
+sudo ln -s $base_path/terminator/share/applications/terminator.desktop $target_path/share/applications
+sudo ln -s $base_path/terminator/share/appdata/terminator.appdata.xml $target_path/share/appdata
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_vert.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_active_broadcast_all.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_active_broadcast_group.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_receive_off.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_receive_on.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_horiz.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/actions/terminator_active_broadcast_off.png $target_path/share/icons/hicolor/16x16/actions/
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/status/terminal-bell.png $target_path/share/icons/hicolor/16x16/status
+sudo ln -s $base_path/terminator/share/icons/hicolor/16x16/apps/terminator.png	$target_path/share/icons/hicolor/16x16/apps
+sudo ln -s $base_path/terminator/share/icons/hicolor/32x32/apps/terminator.png  $target_path/share/icons/hicolor/32x32/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/24x24/apps/terminator.png  $target_path/share/icons/hicolor/24x24/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/48x48/apps/terminator-preferences.png $target_path/share/icons/hicolor/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/48x48/apps/terminator-layout.png	$target_path/share/icons/hicolor/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/48x48/apps/terminator-custom-commands.png	$target_path/share/icons/hicolor/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/48x48/apps/terminator.png	$target_path/share/icons/hicolor/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/22x22/apps/terminator.png	$target_path/share/icons/hicolor/22x22/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/scalable/apps/terminator-custom-commands.svg $target_path/share/icons/hicolor/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/scalable/apps/terminator-layout.svg $target_path/share/icons/hicolor/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/scalable/apps/terminator.svg $target_path/share/icons/hicolor/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/hicolor/scalable/apps/terminator-preferences.svg $target_path/share/icons/hicolor/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_vert.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_active_broadcast_all.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_active_broadcast_group.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_receive_off.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_receive_on.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_horiz.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/actions/terminator_active_broadcast_off.png $target_path/share/icons/HighContrast/16x16/actions
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/status/terminal-bell.png $target_path/share/icons/HighContrast/16x16/status
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/apps/terminator-preferences.png $target_path/share/icons/HighContrast/16x16/apps
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/apps/terminator-layout.png $target_path/share/icons/HighContrast/16x16/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/apps/terminator-custom-commands.png $target_path/share/icons/HighContrast/16x16/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/16x16/apps/terminator.png $target_path/share/icons/HighContrast/16x16/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/32x32/apps/terminator-preferences.png $target_path/share/icons/HighContrast/32x32/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/32x32/apps/terminator-layout.png $target_path/share/icons/HighContrast/32x32/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/32x32/apps/terminator-custom-commands.png $target_path/share/icons/HighContrast/32x32/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/32x32/apps/terminator.png $target_path/share/icons/HighContrast/32x32/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/24x24/apps/terminator-preferences.png $target_path/share/icons/HighContrast/24x24/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/24x24/apps/terminator-layout.png $target_path/share/icons/HighContrast/24x24/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/24x24/apps/terminator-custom-commands.png $target_path/share/icons/HighContrast/24x24/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/24x24/apps/terminator.png $target_path/share/icons/HighContrast/24x24/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/48x48/apps/terminator-preferences.png $target_path/share/icons/HighContrast/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/48x48/apps/terminator-layout.png $target_path/share/icons/HighContrast/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/48x48/apps/terminator-custom-commands.png $target_path/share/icons/HighContrast/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/48x48/apps/terminator.png $target_path/share/icons/HighContrast/48x48/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/22x22/apps/terminator-preferences.png $target_path/share/icons/HighContrast/22x22/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/22x22/apps/terminator-layout.png $target_path/share/icons/HighContrast/22x22/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/22x22/apps/terminator-custom-commands.png $target_path/share/icons/HighContrast/22x22/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/22x22/apps/terminator.png $target_path/share/icons/HighContrast/22x22/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/scalable/apps/terminator-custom-commands.svg $target_path/share/icons/HighContrast/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/scalable/apps/terminator-layout.svg $target_path/share/icons/HighContrast/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/scalable/apps/terminator.svg $target_path/share/icons/HighContrast/scalable/apps/
+sudo ln -s $base_path/terminator/share/icons/HighContrast/scalable/apps/terminator-preferences.svg $target_path/share/icons/HighContrast/scalable/apps/
